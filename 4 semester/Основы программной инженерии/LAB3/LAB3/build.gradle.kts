93793e0765252b1ae9f33be54e81fb71414961a0
Vladimir Kartashev <94760364+ColdDirol@users.noreply.github.com>
2024-06-01 23:51:06 +0300

    upd

--- 4 semester/Основы программной инженерии/LAB3/LAB3/build.gradle.kts
+++ 4 semester/Основы программной инженерии/LAB3/LAB3/build.gradle.kts
@@ -1,3 +1,4 @@
+import java.io.ByteArrayOutputStream
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.Clip
 import javax.sound.sampled.DataLine
@@ -11,20 +12,44 @@ repositories {
     mavenCentral()
 }
 
+// dependencies
+val servletApiVersion: String by properties
+val jakartaWebApiVersion: String by properties
+val primefacesVersion: String by properties
+val postgresqlVersion: String by properties
+val eclipsePersistenceJpaVersion: String by properties
+val junitJupiterApiVersion: String by properties
+val junitJupiterEngineVersion: String by properties
+
+// project paths
+val webAppPath: String by properties
+val webXmlPath: String by properties
+val classesMainPath: String by properties
+val classesTestPath: String by properties
+val mainResourcesPath: String by properties
+val mainWebappPath: String by properties
+
+// jar
+val jarName: String by properties
+val jarVersion: String by properties
+
+// trunk working copy path
+val trunkPath: String by properties
+
 dependencies {
-    providedCompile("javax.servlet:servlet-api:2.5")
-    implementation("jakarta.platform:jakarta.jakartaee-web-api:9.1.0")
-    implementation("org.primefaces:primefaces:13.0.3:jakarta")
-    implementation("org.postgresql:postgresql:42.7.3")
-    implementation("org.eclipse.persistence:org.eclipse.persistence.jpa:4.0.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0-M1")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.0-M1")
+    providedCompile("javax.servlet:servlet-api:${servletApiVersion}")
+    implementation("jakarta.platform:jakarta.jakartaee-web-api:${jakartaWebApiVersion}")
+    implementation("org.primefaces:primefaces:${primefacesVersion}")
+    implementation("org.postgresql:postgresql:${postgresqlVersion}")
+    implementation("org.eclipse.persistence:org.eclipse.persistence.jpa:${eclipsePersistenceJpaVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterApiVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:${junitJupiterEngineVersion}")
 }
 
 tasks.war {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    webAppDirectory = file("src/main/webapp")
-    webXml = file("src/main/webapp/WEB-INF/web.xml") // copies a file to WEB-INF/web.xml
+    webAppDirectory = file(webAppPath)
+    webXml = file(webXmlPath) // copies a file to WEB-INF/web.xml
 }
 
 tasks.register<Test>("lab-test") {
@@ -35,22 +60,35 @@ tasks.register<Test>("lab-test") {
 tasks.register<JavaCompile>("lab-compile") {
     source = sourceSets["main"].java
     classpath = sourceSets["main"].compileClasspath
-    destinationDirectory.set(file("build/classes/java/main"))
+    destinationDirectory.set(file(classesMainPath))
     source = sourceSets["test"].java
     classpath = sourceSets["test"].compileClasspath
-    destinationDirectory.set(file("build/classes/java/test"))
+    destinationDirectory.set(file(classesTestPath))
 }
 
 tasks.register<Jar>("lab-build") {
     dependsOn("lab-compile")
-    from("build/classes/java/main")
-    archiveBaseName.set("LAB3")
-    archiveVersion.set("1.0.0")
+    from(classesMainPath)
+    from(mainResourcesPath)
+    from(mainWebappPath)
+    archiveBaseName.set(jarName)
+    archiveVersion.set(jarVersion)
+    manifest {
+        attributes["Main-Class"] = "Main"
+    }
+
+    doLast {
+        project.exec {
+            commandLine = "echo 'Project have been build'".split(" ")
+        }
+    }
 }
 
 tasks.register("lab-clean") {
     doLast {
         delete("build")
+        delete(".gradle")
+        delete(".database")
     }
 }
 
@@ -68,5 +106,33 @@ tasks.register("lab-music") {
             Thread.sleep(100)
         }
         line.close()
+    }
+}
+
+
+tasks.register("lab-diff") {
+    doLast {
+        val immutableClasses = File("immutable_classes.txt").readLines()
+        println("immutableClasses: $immutableClasses")
+        val byteOut = ByteArrayOutputStream()
+        project.exec {
+            commandLine = "svn status".split(" ")
+            standardOutput = byteOut
+        }
+        val output = byteOut.toByteArray().toString(Charsets.UTF_8)
+        val changedFiles = output.lines().filter { it.startsWith("M ") || it.startsWith("! ") }
+            .map { it.trim().split("\\s+".toRegex(), 2)[1] }
+        println("changedFiles: $changedFiles")
+        if (immutableClasses.any { immutableFile -> changedFiles.any { changedFile -> changedFile.contains(immutableFile) } }) {
+            println("Changes found in classes that should not be changed")
+        } else {
+            val projectDir = file(trunkPath)
+            println("Current directory: ${projectDir.absolutePath}")
+
+            project.exec {
+                workingDir = projectDir
+                commandLine = "sh svn.sh".split(" ")
+            }
+        }
     }
 }
